ded1794088902a02f694c44669fdc477c9656549
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-07-17 11:42:26 +0900

    가은-child_choose tab 추가

--- pages/child_choose.py
+++ pages/child_choose.py
@@ -1,2 +1,14 @@
 #선물로 협상할지 용돈으로 협상할지 선택
 import streamlit as st
+
+st.title('부모님과 협상하기')
+
+tab1, tab2= st.tabs(["Money", "Gift"])
+
+with tab1:
+   st.title('부모님과 용돈 협상하기')
+   st.header("Money")
+
+with tab2:
+   st.title('부모님과 선물 협상하기')
+   st.header("Gift")
